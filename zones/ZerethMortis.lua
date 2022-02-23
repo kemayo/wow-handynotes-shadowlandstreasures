131c1091ce0fa77de6f0704f0ebb8573bbee81a5
David Lynch <kemayo@gmail.com>
2022-02-22 19:20:42 -0600

    Hide the Patient Bufonid before the quest is available

--- zones/ZerethMortis.lua
+++ zones/ZerethMortis.lua
@@ -411,6 +411,7 @@ ns.RegisterPoints(1970, { -- Zereth Mortis
         npc=185798,
         quest={65732, 65724, any=true}, -- 65724 is the daily
         progress={65727, 65725, 65726, 65728, 65729, 65730, 65731},
+        hide_before=65768, -- Our Forward Scouts
         texture=ns.merge(ns.atlas_texture("stablemaster"), {r=1,g=1,b=0,a=1,scale=1.2}),
         minimap=true,
         loot={
