e4928f44bc927ef45c84bc96a83fa6cd4a18312b
David Lynch <kemayo@gmail.com>
2022-03-01 18:29:41 -0600

    Ability to add notes to loot

--- handler/handler.lua
+++ handler/handler.lua
@@ -557,6 +557,9 @@ local function handle_tooltip(tooltip, point)
                         if item.class then
                             link = TEXT_MODE_A_STRING_VALUE_TYPE:format(link, RAID_CLASS_COLORS[item.class]:WrapTextInColorCode(LOCALIZED_CLASS_NAMES_FEMALE[item.class]))
                         end
+                        if item.note then
+                            link = TEXT_MODE_A_STRING_VALUE_TYPE:format(link, item.note)
+                        end
                     end
                     local known = ns.itemIsKnown(item)
                     if known ~= nil and (known == true or not ns.itemRestricted(item)) then
