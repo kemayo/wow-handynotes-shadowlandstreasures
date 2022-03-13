4e5c422304fca25e47e36c5efaaa2cd823020ff6
David Lynch <kemayo@gmail.com>
2022-03-13 15:20:59 -0500

    Disable caches of creation visibility by default

--- shadowlands.lua
+++ shadowlands.lua
@@ -8,3 +8,7 @@ ns.groups["faerietales"] = "{achievement:14788}"
 ns.groups["shardlabor"] = "{achievement:14339}"
 ns.groups["wardrobemakeover"] = "{achievement:14748}"
 ns.groups["sorrow"] = "{achievement:14626}"
+
+ns.defaults.profile.groupsHiddenByZone[1970] = {
+    puzzlecache = true,
+}
