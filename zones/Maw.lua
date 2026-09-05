ce569113b59b7bd16eb35ee29596cafdc4de0117
David Lynch <kemayo@gmail.com>
2026-09-05 16:38:19 -0500

    Maw: fix Valis the Cruel coords
    Per SwiftMint on github

--- zones/Maw.lua
+++ zones/Maw.lua
@@ -440,7 +440,7 @@ ns.RegisterPoints(1543, {
         },
         --level=60,
     },
-    [40695512] = { -- Valis the Cruel
+    [40705962] = { -- Valis the Cruel
         achievement=14660, criteria=49492,
         quest=61728,
         npc=173086,
